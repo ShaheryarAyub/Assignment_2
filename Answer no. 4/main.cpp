3e1ba0f9adcc2b92e552569ee800aedcf761f1ea
ShaheryarAyub <95630185+ShaheryarAyub@users.noreply.github.com>
2021-12-28 12:01:49 +0500

    Update main.cpp

--- Answer no. 4/main.cpp
+++ Answer no. 4/main.cpp
@@ -5,47 +5,39 @@ using namespace std;
 
 int main()
 {
-    Fraction a(5, 3);
-    Fraction b(0, 1);
-    Fraction c;
-    Fraction f1;
-    Fraction f2;
+    Fraction f1(5,3);
+    Fraction f2(2,1);
     Fraction f3;
     Fraction f4;
- 
-    c = a - b;
-    cout << "\n Sum of Two Numbersss : " << c.num << endl;
+    Fraction f5;
 
-    cout << "\n ------------------------- \n";
-   
-    c = b -= a;
-    cout << "\n  operatorrrrr --   : " << c.num << "/" << c.deno;
-    cout << "\n ------------------------- \n";
-    
-    ++a;
-    cout << "\n  operatorrrrr ++   : " << a.num << "/" << a.deno;
-    cout << "\n Enter 1st Fraction Value ";
-    cout << "\n ------------------------- \n";
-
-    f1.accept();
-
-    cout << "\n Enter 2nd Fraction Value ";
-    cout << "\n ------------------------- \n";
-    f2.accept();
-
+    Fraction a(1, 2);
     f3 = f1 + f2;
-    cout << "\n -------------------------------------";
-    cout << "\n Sum of Two Numbers        : " << f3.num << "/" << f3.deno << endl;
-
+    f3.show();
     f3 = f1 - f2;
-    cout << "\n Difference of Two Numbers : " << f3.num << "/" << f3.deno << endl;
-
+    f3.show();
     f3 = f1 * f2;
-    cout << "\n Product of Two Numbers    : " << f3.num << "/" << f3.deno << endl;
-
+    f3.show();
     f3 = f1 / f2;
-    cout << "\n Division of Two Numbers   : " << f3.num << "/" << f3.deno;
-    cout << "\n -------------------------------------";
-   
+    f3.show();
+    f3 = --f1;
+    f3.show();
+    f3 = ++f1;
+    f3.show();
+    f3 =  f1 += a;
+    f3.show();
+    f3 = f1 -= a;
+    f3.show();
+    f3 = -f2;
+    f3.show();
+
+    cout << "\n Enter 1st Fraction Value " << endl;
+    f4.accept();
+    cout << "your input is: ";
+    cout << f4 << endl;
+    cout << "\n Enter 2nd Fraction Value " << endl;
+    cin >> f5;
+    cout << "your input is: ";
+    cout << f5 << endl;
     return 0;
 }
